0932656ae814ef300137d0dda1907e02f0becaba
lorger blaz <lorger.blaz@gmail.com>
2011-02-21 08:39:25 +0100

    Properly implement PeekXmlTask.

---
+++ Flubu.Tests/XmlTests.cs
@@ -0,0 +1,44 @@
+using Flubu.Tasks.Text;
+using MbUnit.Framework;
+
+namespace Flubu.Tests
+{
+    /// <summary>
+    /// Test XML related tasks.
+    /// </summary>
+    [TestFixture]
+    public class XmlTests
+    {
+        [Test]
+        public void FindOne()
+        {
+            ITaskContext c = new TaskContext(new SimpleTaskContextProperties());
+            ITask peek = new PeekXmlTask(SampleXmlFile, "//id", ConfigurationSetting);
+            peek.Execute(c);
+            Assert.AreEqual("2011-02-18_12-16-21", c.Properties[ConfigurationSetting]);
+        }
+
+        [Test]
+        public void FindMultiple()
+        {
+            ITaskContext c = new TaskContext(new SimpleTaskContextProperties());
+            ITask peek = new PeekXmlTask(SampleXmlFile, "//revision", ConfigurationSetting);
+            peek.Execute(c);
+            Assert.AreEqual(
+                new[] {"2200", "https://svn.server/repos/example1/trunk/src2200", "2200",},
+                c.Properties.Get<string[]>(ConfigurationSetting));
+        }
+
+        [Test]
+        public void FindNone()
+        {
+            ITaskContext c = new TaskContext(new SimpleTaskContextProperties());
+            ITask peek = new PeekXmlTask(SampleXmlFile, "/trtwe/rwetr", ConfigurationSetting);
+            peek.Execute(c);
+            Assert.IsNull(c.Properties[ConfigurationSetting]);
+        }
+
+        private const string SampleXmlFile = "hudsonBuildSample.xml";
+        private const string ConfigurationSetting = "result-34";
+    }
+}
