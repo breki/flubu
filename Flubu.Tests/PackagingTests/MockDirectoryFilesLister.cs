402c9b36c42ef1fceed616d23120d175a29849b4
brejc <226863+breki@users.noreply.github.com>
2012-11-12 10:53:19 +0100

    Added AddTransformationWithDirFlattening method to CopyProcessor. Added a couple of tests for CopyProcessor.

---
+++ Flubu.Tests/PackagingTests/MockDirectoryFilesLister.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Flubu.Packaging;
+
+namespace Flubu.Tests.PackagingTests
+{
+    public class MockDirectoryFilesLister : IDirectoryFilesLister
+    {
+        public MockDirectoryFilesLister List(string fileName)
+        {
+            filesToList.Add(fileName);
+            return this;
+        }
+
+        public IEnumerable<string> ListFiles(string directoryName, bool recursive)
+        {
+            foreach (string file in filesToList)
+                yield return Path.Combine(directoryName, file);
+        }
+
+        private List<string> filesToList = new List<string>();
+    }
+}
