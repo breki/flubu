a223646f49f74b908d8882993184e13e2ba6d5a3
igor <226863+breki@users.noreply.github.com>
2016-01-07 10:25:36 +0100

    2.4.0.0 - CompileSolutionTask enhancements by Toma? Zavr?nik

--- Flubu/Builds/Tasks/SolutionTasks/CompileSolutionTask.cs
+++ Flubu/Builds/Tasks/SolutionTasks/CompileSolutionTask.cs
@@ -5,7 +5,10 @@ namespace Flubu.Builds.Tasks.SolutionTasks
 {
     public class CompileSolutionTask : TaskBase
     {
-        public CompileSolutionTask (string solutionFileName, string buildConfiguration, string dotNetVersion)
+        public CompileSolutionTask (
+            string solutionFileName, 
+            string buildConfiguration, 
+            string dotNetVersion)
         {
             this.solutionFileName = solutionFileName;
             this.buildConfiguration = buildConfiguration;
@@ -17,21 +20,40 @@ namespace Flubu.Builds.Tasks.SolutionTasks
             get { return "Compile VS solution"; }
         }
 
-        public string Target
-        {
-            get { return target; }
-            set { target = value; }
-        }
-
         public int MaxCpuCount
         {
             get { return maxCpuCount; }
             set { maxCpuCount = value; }
         }
 
+        public string Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+
+        public string ToolsVersion
+        {
+            get { return toolsVersion; }
+            set { toolsVersion = value; }
+        }
+
+        public bool UseSolutionDirAsWorkingDir
+        {
+            get { return useSolutionDirAsWorkingDir; }
+            set { useSolutionDirAsWorkingDir = value; }
+        }
+
         protected override void DoExecute (ITaskContext context)
         {
-            string msbuildPath = FlubuEnvironment.GetDotNetFWDir (dotNetVersion);
+            string msbuildPath;
+
+            bool isDifferentToolsVersion = !string.IsNullOrEmpty(toolsVersion) && !dotNetVersion.Equals(toolsVersion);
+
+            if (isDifferentToolsVersion)
+                msbuildPath = FlubuEnvironment.GetDotNetFWDir (toolsVersion);
+            else
+                msbuildPath = FlubuEnvironment.GetDotNetFWDir (dotNetVersion);
 
             RunProgramTask task = new RunProgramTask (Path.Combine (msbuildPath, @"msbuild.exe"), false);
             task
@@ -40,6 +62,13 @@ namespace Flubu.Builds.Tasks.SolutionTasks
                 .AddArgument ("/p:Platform=Any CPU")
                 .AddArgument ("/consoleloggerparameters:NoSummary")
                 .AddArgument ("/maxcpucount:{0}", maxCpuCount);
+
+            if (isDifferentToolsVersion)
+                task.AddArgument("/toolsVersion:{0}", toolsVersion.Substring(1));
+ 
+            if (useSolutionDirAsWorkingDir)
+                task.SetWorkingDir(Path.GetDirectoryName(solutionFileName));
+
             if (target != null)
                 task.AddArgument ("/t:{0}", target);
 
@@ -50,6 +79,8 @@ namespace Flubu.Builds.Tasks.SolutionTasks
         private readonly string solutionFileName;
         private readonly string buildConfiguration;
         private readonly string dotNetVersion;
+        private string toolsVersion;
+        private bool useSolutionDirAsWorkingDir;
         private int maxCpuCount = 3;
     }
 }
