b81e127ee072ec783704f2d4b0e10797e986a754
Marko Zorec <mzorec@gmail.com>
2016-01-18 15:07:16 +0100

    ...

--- Flubu.Contrib/Tasks/Iis/MimeType.cs
+++ Flubu.Contrib/Tasks/Iis/MimeType.cs
@@ -8,12 +8,12 @@ namespace Flubu.Tasks.Iis
     public class MimeType
     {
         /// <summary>
-        /// File extension of the mime type.
+        /// Gets or sets File extension of the mime type.
         /// </summary>
         public string FileExtension { get; set; }
         
         /// <summary>
-        /// The Mime type.
+        /// Gets or sets The Mime type.
         /// </summary>
         public string Type { get; set; }
     }
