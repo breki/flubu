099714a2a3c425b0d97b059e9996c5deba66f601
igor <igor@localhost>
2016-07-13 18:02:39 +0200

    3.3.0.0 - Refactored Console code to be testable and added tests for it. - Console will no longer execute the first .cs file in the current directory since this was a very bad idea.

---
+++ Flubu.Console/BuildScriptLocatorException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Flubu.Console
+{
+    [Serializable]
+    public class BuildScriptLocatorException : Exception
+    {
+        public BuildScriptLocatorException(string message)
+            : base(message)
+        {
+        }
+    }
+}
